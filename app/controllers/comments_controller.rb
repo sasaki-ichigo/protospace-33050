871be2308ec909b3ee0477b8c4e328b2fe77bd9d
sasaki-ichigo <rockthebeat15@docomo.ne.jp>
2020-12-29 20:27:03 +0900

    commentsコントローラーのprivateメソッドにストロングパラメーターをセットし、特定の値のみを受け付けるようにした。且つ、user_idとprototype_idもmergeした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,8 @@ class CommentsController < ApplicationController
     
   end
 
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 end

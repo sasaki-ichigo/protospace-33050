b96a49820961073bec154667e92d8ee585edffaf
sasaki-ichigo <rockthebeat15@docomo.ne.jp>
2020-12-29 17:18:55 +0900

    updateアクションに、データが更新されなかったときは、編集ページに戻るようにrenderを用いて記述した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,11 +27,10 @@ class PrototypesController < ApplicationController
 
   def update
     prototype = Prototype.find(params[:id])
-    @prototype = prototype.update(prototype_params)
-    if @prototype.save
-      redirect_to prototype_path(prototype.id)
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
     else
-      render :edit(prototype.id)
+      render :edit
     end
   end
 
